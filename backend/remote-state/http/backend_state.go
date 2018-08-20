f72626dfddea8abc8d1a6aa9cd610d4e60498423
Stefan Oprea <soprea@wayfair.com>
2018-08-20 10:02:45 +0200

    Testing

--- backend/remote-state/http/backend_state.go
+++ backend/remote-state/http/backend_state.go
@@ -1,17 +1,17 @@
-package http
+package gcs
 
 import (
-	"bytes"
-	"errors"
 	"fmt"
-	"path/filepath"
+	"path"
 	"sort"
 	"strings"
 
+	"cloud.google.com/go/storage"
 	"github.com/hashicorp/terraform/backend"
 	"github.com/hashicorp/terraform/state"
 	"github.com/hashicorp/terraform/state/remote"
 	"github.com/hashicorp/terraform/terraform"
+	"google.golang.org/api/iterator"
 )
 
 const (
@@ -19,156 +19,141 @@ const (
 	lockFileSuffix  = ".tflock"
 )
 
-// get a list of all states.
+// States returns a list of names for the states found on GCS. The default
+// state is always returned as the first element in the slice.
 func (b *Backend) States() ([]string, error) {
-	var result []string
-	client := &RemoteClient{
-		client:   b.client,
-		address:  b.address,
-		username: b.username,
-		password: b.password,
-	}
+	states := []string{backend.DefaultStateName}
 
-	resp, err := client.Get()
-	if err != nil {
-		return nil, err
-	}
-	// Read in the body
-	// Get all the data sent by REST
-	buff := string(resp.Data)
-	buff = strings.Replace(buff, ",", " ", -1)
-	buffSlice := strings.Fields(buff)
-  sort.Strings(buffSlice)
-	for _, el := range buffSlice {
-		// iterate on reposne and make sure we only get .tfstate files
-		// This should be implemented in REST API but just to be safe.
-		fName := filepath.Base(el)
-		extName := filepath.Ext(el)
-		bname := fName[:len(fName)-len(extName)]
-		if extName == stateFileSuffix {
-			result = append(result, bname)
+	bucket := b.storageClient.Bucket(b.bucketName)
+	objs := bucket.Objects(b.storageContext, &storage.Query{
+		Delimiter: "/",
+		Prefix:    b.prefix,
+	})
+	for {
+		attrs, err := objs.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("querying Cloud Storage failed: %v", err)
+		}
+
+		name := path.Base(attrs.Name)
+		if !strings.HasSuffix(name, stateFileSuffix) {
+			continue
+		}
+		st := strings.TrimSuffix(name, stateFileSuffix)
+
+		if st != backend.DefaultStateName {
+			states = append(states, st)
 		}
 	}
 
-	//sort.Strings(result[1:])
-if sort.SearchStrings(results, "default"){
-  result = append(result, "default")
-}
-	return result, nil
+	sort.Strings(states[1:])
+	return states, nil
 }
 
+// DeleteState deletes the named state. The "default" state cannot be deleted.
 func (b *Backend) DeleteState(name string) error {
-	if name == backend.DefaultStateName || name == "" {
-		return fmt.Errorf("can't delete default state")
+	if name == backend.DefaultStateName {
+		return fmt.Errorf("cowardly refusing to delete the %q state", name)
 	}
-	client, err := b.remoteClient(name)
+
+	c, err := b.client(name)
 	if err != nil {
 		return err
 	}
 
-	return client.Delete()
+	return c.Delete()
 }
 
-// get a remote client configured for this state
-func (b *Backend) remoteClient(name string) (*RemoteClient, error) {
+// client returns a remoteClient for the named state.
+func (b *Backend) client(name string) (*remoteClient, error) {
 	if name == "" {
-		return nil, errors.New("missing state name")
+		return nil, fmt.Errorf("%q is not a valid state name", name)
 	}
-	client := &RemoteClient{
-		client:        b.client,
-		address:       b.statePath(name),
-		updateMethod:  b.updateMethod,
-		lockAddress:   b.lockPath(name),
-		unlockAddress: b.lockPath(name),
-		lockMethod:    b.lockMethod,
-		unlockMethod:  b.unlockMethod,
-		username:      b.username,
-		password:      b.password,
-	}
-	return client, nil
+
+	return &remoteClient{
+		storageContext: b.storageContext,
+		storageClient:  b.storageClient,
+		bucketName:     b.bucketName,
+		stateFilePath:  b.stateFile(name),
+		lockFilePath:   b.lockFile(name),
+		encryptionKey:  b.encryptionKey,
+	}, nil
 }
 
+// State reads and returns the named state from GCS. If the named state does
+// not yet exist, a new state file is created.
 func (b *Backend) State(name string) (state.State, error) {
-	client, err := b.remoteClient(name)
+	c, err := b.client(name)
 	if err != nil {
 		return nil, err
 	}
 
-	stateMgr := &remote.State{Client: client}
+	st := &remote.State{Client: c}
 
 	// Grab the value
-	if err := stateMgr.RefreshState(); err != nil {
+	if err := st.RefreshState(); err != nil {
 		return nil, err
 	}
 
 	// If we have no state, we have to create an empty state
-	if v := stateMgr.State(); v == nil {
-		// take a lock on this state while we write it
+	if v := st.State(); v == nil {
+
 		lockInfo := state.NewLockInfo()
 		lockInfo.Operation = "init"
-		lockId, err := client.Lock(lockInfo)
-
+		lockID, err := st.Lock(lockInfo)
 		if err != nil {
-			return nil, fmt.Errorf("failed to lock http state: %s", err)
+			return nil, err
 		}
 
 		// Local helper function so we can call it multiple places
-		lockUnlock := func(parent error) error {
-			if err := stateMgr.Unlock(lockId); err != nil {
-				return fmt.Errorf(strings.TrimSpace(errStateUnlock), lockId, err)
+		unlock := func(baseErr error) error {
+			if err := st.Unlock(lockID); err != nil {
+				const unlockErrMsg = `%v
+				Additionally, unlocking the state file on Google Cloud Storage failed:
+
+				Error message: %q
+				Lock ID (gen): %v
+				Lock file URL: %v
+
+				You may have to force-unlock this state in order to use it again.
+				The GCloud backend acquires a lock during initialization to ensure
+				the initial state file is created.`
+				return fmt.Errorf(unlockErrMsg, baseErr, err.Error(), lockID, c.lockFileURL())
 			}
-			return parent
+
+			return baseErr
 		}
 
-		if err := stateMgr.WriteState(terraform.NewState()); err != nil {
-			err = lockUnlock(err)
-			return nil, err
+		if err := st.WriteState(terraform.NewState()); err != nil {
+			return nil, unlock(err)
 		}
-		if err := stateMgr.PersistState(); err != nil {
-			err = lockUnlock(err)
-			return nil, err
+		if err := st.PersistState(); err != nil {
+			return nil, unlock(err)
 		}
 
 		// Unlock, the state should now be initialized
-		if err := lockUnlock(nil); err != nil {
+		if err := unlock(nil); err != nil {
 			return nil, err
 		}
+
 	}
-	return stateMgr, nil
+
+	return st, nil
 }
 
-// Construct the path of the state file based on named state
-func (b *Backend) statePath(name string) string {
-	paths := []string{b.address, "/", name, stateFileSuffix}
-	var buf bytes.Buffer
-	for _, p := range paths {
-		buf.WriteString(p)
+func (b *Backend) stateFile(name string) string {
+	if name == backend.DefaultStateName && b.defaultStateFile != "" {
+		return b.defaultStateFile
 	}
-	path := buf.String()
-
-	return path
+	return path.Join(b.prefix, name+stateFileSuffix)
 }
 
-// Construct the path of the lock file based on named state
-func (b *Backend) lockPath(name string) string {
-	paths := []string{b.address, "/", name, lockFileSuffix}
-	var buf bytes.Buffer
-	for _, p := range paths {
-		buf.WriteString(p)
+func (b *Backend) lockFile(name string) string {
+	if name == backend.DefaultStateName && b.defaultStateFile != "" {
+		return strings.TrimSuffix(b.defaultStateFile, stateFileSuffix) + lockFileSuffix
 	}
-	path := buf.String()
-
-	return path
+	return path.Join(b.prefix, name+lockFileSuffix)
 }
-
-func (b *Backend) Client() *RemoteClient {
-	return &RemoteClient{}
-}
-
-const errStateUnlock = `
-Error unlocking http state. Lock ID: %s
-
-Error: %s
-
-You may have to force-unlock this state in order to use it again.
-`
